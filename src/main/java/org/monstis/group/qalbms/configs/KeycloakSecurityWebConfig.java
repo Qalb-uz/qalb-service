9502464992d7a61a4b5d17da7818f4c5b191aa5a
SaidJumayev19980828 <jumayevsaid679@gmail.com>
2025-04-05 21:17:33 +0500

    fixing issues

--- src/main/java/org/monstis/group/qalbms/configs/KeycloakSecurityWebConfig.java
+++ src/main/java/org/monstis/group/qalbms/configs/KeycloakSecurityWebConfig.java
@@ -25,7 +25,7 @@ public class KeycloakSecurityWebConfig {
 		http.csrf(ServerHttpSecurity.CsrfSpec::disable)
 				.authorizeExchange(exchange -> exchange.pathMatchers("/admin/**").hasRole("ADMIN")
 						.pathMatchers("/user/**").hasRole("USER")
-						.pathMatchers("/api/v1/auth/refresh-token","webjars/swagger-ui/index.html","/api/register/**","/api/verify-otp/**").permitAll()
+						.pathMatchers("/api/v1/auth/refresh-token","webjars/swagger-ui/index.html","v1/swagger-ui/index.html","/api/register/**","/api/verify-otp/**").permitAll()
 						.pathMatchers("/swagger-ui/**","/v3/api-docs/**","swagger-ui.html","v1/api-docs/**", "/api-docs/**", "/swagger-resources/**", "/webjars/**")
 						.permitAll()
 						.anyExchange().authenticated())
